ee7e5a0f914dcb5f8464c0286e147a896e734068
l <vule59695@gmail.com>
2026-07-19 20:05:38 +0700

    Initial Commit

---
+++ AppLearnEnglish/AppLearnEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLearnEnglishApp.swift
+//  AppLearnEnglish
+//
+//  Created by Lê Đức Vũ on 19/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AppLearnEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
